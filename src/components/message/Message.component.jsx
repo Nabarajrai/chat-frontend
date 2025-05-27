554fbee0b5b29efc9bcbb6597b848654bf6e0484
Nabarajrai <nabaraj2055@gmail.com>
2025-05-27 20:32:00 +0545

    rename in classname

--- src/components/message/Message.component.jsx
+++ src/components/message/Message.component.jsx
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 //helpers
 import { sanitizeHtml } from "../../helpers/SafeHtml.helper";
 import { useMessageContext } from "../../context/message/Message.context";
@@ -7,47 +7,97 @@ import { useTabsContext } from "../../context/tabs/Tabs.context";
 const MessageComponent = () => {
   const { messages } = useMessageContext();
   const { tabName } = useTabsContext();
+  const [array, setArray] = useState([
+    {
+      id: 1,
+      data: [
+        { messageId: 1, text: "<p>Message 1</p>", isEdited: true },
+        { messageId: 2, text: "<p>Message 2</p>", isEdited: true },
+        { messageId: 3, text: "<p>Message 3</p>", isEdited: true },
+      ],
+      receiverId: 1,
+      senderId: 2,
+    },
+    {
+      id: 2,
+      data: [
+        { messageId: 1, text: "<p>Message 1</p>", isEdited: true },
+        { messageId: 2, text: "<p>Message 2</p>", isEdited: true },
+        { messageId: 3, text: "<p>Message 3</p>", isEdited: true },
+      ],
+      receiverId: 2,
+      senderId: 2,
+    },
+    {
+      id: 3,
+      data: [
+        { messageId: 1, text: "<p>Message 1</p>", isEdited: true },
+        { messageId: 2, text: "<p>Message 2</p>", isEdited: true },
+        { messageId: 3, text: "<p>Message 3</p>", isEdited: true },
+      ],
+      receiverId: 1,
+      senderId: 2,
+    },
+    {
+      id: 3,
+      data: [
+        { messageId: 1, text: "<p>Message 1</p>", isEdited: true },
+        { messageId: 2, text: "<p>Message 2</p>", isEdited: true },
+        { messageId: 3, text: "<p>Message 3</p>", isEdited: true },
+      ],
+      receiverId: 1,
+      senderId: 2,
+    },
+    {
+      id: 3,
+      data: [
+        { messageId: 1, text: "<p>Message 1</p>", isEdited: true },
+        { messageId: 2, text: "<p>Message 2</p>", isEdited: true },
+        { messageId: 3, text: "<p>Message 3</p>", isEdited: true },
+      ],
+      receiverId: 1,
+      senderId: 2,
+    },
+  ]);
   console.log("tabName", tabName);
   return (
-    <div className="content-section">
-      <div className="dashboard-header">
+    <div className="message-section">
+      <div className="message-header">
         <h1># {tabName}</h1>
       </div>
-      <div className="dashboard-body-content">
-        <div className="dashboard-body">
-          <div className="dashboard-body__message--user">
-            <div className="dashboard-body__message--avatar">
-              <img
-                src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
-                alt=""
-              />
-            </div>
-            <div className="dashboard-body__message--name">
-              <span>John Doe</span>
-            </div>
-          </div>
-          <div className="dashboard-body__message--content">
-            {messages.map((msg) => {
-              return (
-                <div key={msg?.id}>
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: sanitizeHtml(msg?.data),
-                    }}
+      <div className="message-lists">
+        {array.map((msg) => {
+          return (
+            <div className="message-wrapper" key={msg.id}>
+              <div className="message-user">
+                <div className="message-user__avatar">
+                  <img
+                    src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+                    alt=""
                   />
                 </div>
-              );
-            })}
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-              voluptatem.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-              voluptatem.
-            </p>
-          </div>
-        </div>
+                <div className="message-user__name">
+                  <span>John Doe</span>
+                </div>
+              </div>
+              <div className="message-body-wrapper">
+                {msg.data.map((message) => (
+                  <div
+                    key={message.messageId}
+                    className={`message-body ${
+                      message.isEdited ? "edited" : ""
+                    }`}>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: sanitizeHtml(message.text),
+                      }}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
